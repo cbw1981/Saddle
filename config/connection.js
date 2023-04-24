74b82a0c30c172d11991d165dec706cbc92ff4be
Cbw1981 <braxtonwilkey@gmail.com>
2023-04-24 19:39:07 -0400

    fixed database

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URL)
+mongoose.connect(process.env.MONGO_URL);
 
 const connection = mongoose.connection;
 
